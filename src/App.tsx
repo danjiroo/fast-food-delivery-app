945609c15c9b17289c744151b9ff4e8e1f19c599
Dan Quesada <danilo.q@pandolink.com>
2023-02-12 22:02:53 +0800

    Update: fixed wizard not seen in mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { MultiStepForm } from './pages'
 
 const App: React.FC = () => (
-  <div className='h-screen w-screen flex flex-col overflow-hidden md:flex-row'>
+  <div className='h-full w-screen flex flex-col overflow-hidden md:flex-row'>
     <div className='w-full h-[20vh] bg-banner bg-center md:h-full md:w-1/2'></div>
     <MultiStepForm />
   </div>
